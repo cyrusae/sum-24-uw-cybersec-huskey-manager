91dd53a63f0fbaba6677626a90ffe99d53598740
Cyrus <86699936+cyrusae@users.noreply.github.com>
2024-07-08 17:44:56 -0700

    Tentatively add BruteForceBlock??

--- database/init.sql
+++ database/init.sql
@@ -128,3 +128,14 @@ VALUES
     (11, 4, 3, 1), -- Jane Doe (Owner)
     (12, 9, 3, 2), -- Emily Davis (Editor)
     (13, 12, 3, 3) -- Chris Miller (Viewer)
+
+--create additional logging database
+--see: https://github.com/ejfrancis/brute-force-block
+CREATE TABLE IF NOT EXISTS user_failed_logins (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  user_id bigint(20) NOT NULL,
+  ip_address int(11) unsigned DEFAULT NULL,
+  attempted_at datetime NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+) ;
