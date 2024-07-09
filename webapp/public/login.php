7c93abd3196cf5b14c3dee652e72c00640f28dd7
Cyrus <86699936+cyrusae@users.noreply.github.com>
2024-07-08 17:03:25 -0700

    Make the sql m a r g i n a l l y less cursed

--- webapp/public/login.php
+++ webapp/public/login.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password' AND approved = 1";
+    $sql = "SELECT username FROM users WHERE username = '$username' AND password = '$password' AND approved = 1";
     $result = $conn->query($sql);
 
     if($result->num_rows > 0) {
