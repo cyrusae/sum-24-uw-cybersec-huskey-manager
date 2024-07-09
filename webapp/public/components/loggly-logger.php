a7c1a7cd127f66074dff0c63864997553af46221
Cyrus <86699936+cyrusae@users.noreply.github.com>
2024-07-09 14:06:52 -0700

    Learn exciting problems about PHP logging

--- webapp/public/components/loggly-logger.php
+++ webapp/public/components/loggly-logger.php
@@ -15,4 +15,3 @@ $logger->info('Loggly Sending Informational Message');
 
 //TODO: Actually take some kind of bloody advantage of the fact that this is sending JSON instead of barfing up verbose text only
 ?>
-
