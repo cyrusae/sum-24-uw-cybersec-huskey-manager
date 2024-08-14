ef150007063dd1466543dea07f4a4842a670905e
Cyrus <86699936+cyrusae@users.noreply.github.com>
2024-08-14 16:27:52 -0700

    Update login_behavior.php

--- webapp/public/components/experimental/login_behavior.php
+++ webapp/public/components/experimental/login_behavior.php
@@ -1,18 +1,19 @@
 <?php
 $sql = $conn->prepare("SELECT password FROM users WHERE username = ? AND approved = 1");
 $username = $_POST['username'];
-$sql->bind_param("s", $username); 
+$sql->bind_param("s", $username); //use $username in the query 
 $sql->execute();
-$sql->bind_result($password_expected);
-$sql->fetch();
+$sql->bind_result($password_expected); //hold the retrieved password (which, ideally, is hashed)
+$sql->fetch(); //tutorials say this is needed to actually pull the value out? is that true
+
 //add exit/failure on not locating existing user here
 
-$algo = PASSWORD_DEFAULT;
+$algo = PASSWORD_DEFAULT; //PHP will update our hashing to better encryption algorithms as the language gets new ones
 $options = ['cost' => 10];
 
 $password = $_POST['password'];
 
-if (password_verify($password, $password_expected) || ($password === $password_expected)) {
+if (password_verify($password, $password_expected) || ($password === $password_expected)) { //Is the password either actually the right hash (desired behavior) or the plaintext version (we can handle that...)
  //Password is valid, proceed with login
  $hashed_password = password_hash($password, $algo, $options);
  if (password_needs_rehash($password_expected, $algo, $options) || ($password === $password_expected)) {
