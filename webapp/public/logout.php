1422c7f9cfed6332b5e865d1fc91a92bf0b0d16d
Cyrus <86699936+cyrusae@users.noreply.github.com>
2024-08-14 20:13:11 -0700

    Update logout.php

--- webapp/public/logout.php
+++ webapp/public/logout.php
@@ -5,16 +5,10 @@ include './components/loggly-logger.php';
 
 include './components/console-logger.php';
 
-
- 
-
-$logger->info('Session ended for user ' .  $username); 
-
 // Redirect to the login page
 
 session_unset();
 session_destroy();
-session_regenerate_id(true);
 header('Location: /login.php');
 exit();
 
